498e1abdcc08b69be77dbb526979d1c3d8f44eca
bjg5k2 <31576875+bjg5k2@users.noreply.github.com>
2025-09-30 18:19:10 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+window.appConfig = {
+  firebase: {
+    apiKey: AIzaSyBF5gzPThKD1ga_zpvtdBpiQFsexbEpZyY,
+  authDomain: stockify-75531.firebaseapp.com,
+  projectId: stockify-75531,
+  storageBucket: stockify-75531.firebasestorage.app,
+  messagingSenderId: 831334536771,
+  appId: 1:831334536771:web:b142abcead4df128c826f6
+  },
+  spotify: {
+    clientId: b0450273fe7d41a08cc3ea93a2e733ae,
+    clientSecret: 5b22a59a771b4f8885f887958bfddeb2
+  }
+};
