80b6df66ca7c64eab2baf31c878dccca769f09cb
bjg5k2 <31576875+bjg5k2@users.noreply.github.com>
2025-09-30 18:39:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
+// config.js — Fill in your own Firebase and Spotify credentials
 window.appConfig = {
   firebase: {
-    apiKey: AIzaSyBF5gzPThKD1ga_zpvtdBpiQFsexbEpZyY,
-  authDomain: stockify-75531.firebaseapp.com,
-  projectId: stockify-75531,
-  storageBucket: stockify-75531.firebasestorage.app,
-  messagingSenderId: 831334536771,
-  appId: 1:831334536771:web:b142abcead4df128c826f6
+    apiKey: "AIzaSyBF5gzPThKD1ga_zpvtdBpiQFsexbEpZyY",
+  authDomain: "stockify-75531.firebaseapp.com",
+  projectId: "stockify-75531",
+  storageBucket: "stockify-75531.firebasestorage.app",
+  messagingSenderId: "831334536771",
+  appId: "1:831334536771:web:b142abcead4df128c826f6"
   },
   spotify: {
-    clientId: b0450273fe7d41a08cc3ea93a2e733ae,
-    clientSecret: 5b22a59a771b4f8885f887958bfddeb2
+    clientId: "b0450273fe7d41a08cc3ea93a2e733ae",
+    clientSecret: "5b22a59a771b4f8885f887958bfddeb2"
   }
 };
